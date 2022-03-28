ef9400e07426b2f16a905a507e9531e2b99ef2d3
clarkspencerc <clarkspencerc@gmail.com>
2022-03-27 21:32:05 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,14 +5,11 @@ var playerHealth = 100;
 var playerAttack= 10; 
 var playerMoney = 10; 
 
-// You can also log multiple values at once like this 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto"; 
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];  
 var enemyHealth = 50; 
 var enemyAttack = 12; 
 
-var fight = function() {
+var fight = function(enemyName) {
     // alert players that they are starting the round 
     window.alert("Welcome to Robot Gladiators"); 
 
@@ -49,18 +46,35 @@ var fight = function() {
         else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
-        // if player choses to skip 
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert(playerName + " has chosen to skip the fight!"); 
-    } else {
+    } 
+    // if player choses to skip 
+    else if (promptFight === "skip" || promptFight === "SKIP") {
+        // confirm player wants to skip 
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?"); 
+
+        // if yes (true), leave fight 
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            // subtract money from the playerMoney for skipping 
+            playerMoney = playerMoney -2; 
+        }
+        // if no (false), ask question again by running fight() again 
+        else{
+            fight(); 
+        }
+    } 
+    else {
         window.alert("You need to choose a valid option. Try again"); 
     }
-
-
-    
-
-   
-
 }; 
 
-fight(); 
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
+// fight(); 
+
+// Game States 
+// "WIN" - Player robot has defeated all enemy-robots 
+//  * Fight all enemy-robots 
+//  * Defeat each enemy-robot 
+// "LOSE" - Player robot's health is zero or less 
